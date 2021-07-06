0fb60d2a35e769357b02744732a184361fd43b96
sagarmundkar <48950343+sagarmundkar@users.noreply.github.com>
2021-07-06 16:49:37 +0530

    Update Logout.java

--- src/main/java/com/selenium/test/Pages/Logout.java
+++ src/main/java/com/selenium/test/Pages/Logout.java
@@ -2,12 +2,29 @@ package com.selenium.test.Pages;
 
 import com.selenium.test.base.BaseProgram;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class Logout extends BaseProgram {
-    public void logout() {
-       /* driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[4]/div[1]/span[1]/div[1]/div[1]/i[1]")).click();
-        driver.findElement(By.xpath("//span[contains(text(),'Log Out')]")).click();
-
-        System.out.println("Logout Successfully!");*/
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[4]/div[1]/span[1]/div[1]/div[1]/i[1]")
+    WebElement Account;
+    
+    public Logout(WebDriver driver) {
+        PageFactory.initElements(driver, this);
     }
+
+    public void logout() throws InterruptedException {
+        Account.click();
+        Thread.sleep(1000);
+        System.out.println("Logout Successfully!");
+
+        String actualurl = "https://www.facebook.com/";
+        String expectedurl = driver.getCurrentUrl();
+        Assert.assertEquals(expectedurl,actualurl);
+
+    }
+
 }
