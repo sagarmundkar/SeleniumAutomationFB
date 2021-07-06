4a7469ef048269b2926f2986e186e0a70a5a8bf8
sagarmundkar <48950343+sagarmundkar@users.noreply.github.com>
2021-07-06 16:51:22 +0530

    Update Screenshot.java

--- src/main/java/utility/Screenshot.java
+++ src/main/java/utility/Screenshot.java
@@ -1,17 +1,21 @@
-package com.selenium.test.Pages;
+package utility;
 
 import com.selenium.test.base.BaseProgram;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.testng.annotations.Listeners;
+import utility.Listerners.ListenerTest;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 
+@Listeners(ListenerTest.class)
 public class Screenshot extends BaseProgram {
 
-    public void screenshot(String screenshotName) throws IOException {
+    public void screenshot(String screenshotName,String Result) throws IOException {
+
         Date d = new Date();
         //printing actual date
         String date = d.toString();
@@ -21,13 +25,13 @@ public class Screenshot extends BaseProgram {
         System.out.println(date1);
 
         driver.get("https://www.facebook.com");
+        //Typecating driver object to takescreenshot interface
         TakesScreenshot ts = (TakesScreenshot) driver;
+        //getting the source file using getScreenShot
         File srcFile = ts.getScreenshotAs(OutputType.FILE);
 
         //create screenshot folder in project directory
-        File destFile = new File(".\\screenshot\\"+date1+"\\"+screenshotName+".png");
+        File destFile = new File(".\\screenshot\\"+Result+"\\"+screenshotName+"_"+date1+".png");
         FileUtils.copyFile(srcFile,destFile);
     }
-
-
 }
