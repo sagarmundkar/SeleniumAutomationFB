579869002ebca90b82a863f05e9c25002f2e3dea
sagarmundkar <48950343+sagarmundkar@users.noreply.github.com>
2021-07-06 16:48:37 +0530

    Update PostPage.java

--- src/main/java/com/selenium/test/Pages/PostPage.java
+++ src/main/java/com/selenium/test/Pages/PostPage.java
@@ -1,28 +1,44 @@
 package com.selenium.test.Pages;
 
 import com.selenium.test.base.BaseProgram;
+import org.checkerframework.checker.units.qual.K;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
 public class PostPage extends BaseProgram {
 
-      /* @FindBy(className = "a8c37x1j")
-       WebElement create;
-
-        @FindBy(xpath = "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[2]/div/div/div/div[1]/div/div[1]/span")
-        WebElement createpost;
+        @FindBy(xpath = "//span[contains(text(),'Photo/Video')]")
+        WebElement CreatePost;
 
         public  PostPage(WebDriver driver){
             PageFactory.initElements(driver,this);
         }
-        public void Post_to_page() throws InterruptedException {
-         //   create.click();
-           // Thread.sleep(200);
-            createpost.click();
-           Thread.sleep(1000);
-    }*/
 
+        public void uploadFile() throws AWTException {
+            StringSelection selection = new StringSelection("C:\\Users\\Administrator\\Pictures\\Nature.jpg");
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection,null);
+            //Create robot class object for keyboard and mouse handle
+            Robot r = new Robot();
+            r.keyPress(KeyEvent.VK_CONTROL);
+            r.keyPress(KeyEvent.VK_V);
+            r.delay(1000);
+            r.keyRelease(KeyEvent.VK_CONTROL);
+            r.keyRelease(KeyEvent.VK_V);
+            r.delay(1000);
+            r.keyPress(KeyEvent.VK_ENTER);
+            r.keyRelease(KeyEvent.VK_ENTER);
+            r.delay(1000);
+        }
 
+        public void Post_to_page() throws InterruptedException, AWTException {
+            CreatePost.click();
+            Thread.sleep(2000);
+            uploadFile();
+    }
 }
