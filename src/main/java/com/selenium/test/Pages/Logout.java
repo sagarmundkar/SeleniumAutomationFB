0325f4d3bbc0827103335248c78374bc86bf9d89
sagarmundkar <48950343+sagarmundkar@users.noreply.github.com>
2021-07-07 16:08:43 +0530

    Updated logout file  with Log4j

--- src/main/java/com/selenium/test/Pages/Logout.java
+++ src/main/java/com/selenium/test/Pages/Logout.java
@@ -1,7 +1,8 @@
 package com.selenium.test.Pages;
 
 import com.selenium.test.base.BaseProgram;
-import org.openqa.selenium.By;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,14 +10,17 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
 public class Logout extends BaseProgram {
+    Logger logger = LogManager.getLogger(Login.class);
+
     @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[4]/div[1]/span[1]/div[1]/div[1]/i[1]")
     WebElement Account;
-    
+
     public Logout(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
 
     public void logout() throws InterruptedException {
+        logger.debug("Click on Logout!");
         Account.click();
         Thread.sleep(1000);
         System.out.println("Logout Successfully!");
