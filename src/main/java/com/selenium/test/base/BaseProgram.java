ed5d442ecb5b65c6da3812d946649d54b6db0997
sagarmundkar <48950343+sagarmundkar@users.noreply.github.com>
2021-07-07 16:11:55 +0530

    Updated BaseProgram file with Log4j

--- src/main/java/com/selenium/test/base/BaseProgram.java
+++ src/main/java/com/selenium/test/base/BaseProgram.java
@@ -1,6 +1,9 @@
 package com.selenium.test.base;
 
+import com.selenium.test.Pages.Login;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -8,19 +11,29 @@ import org.testng.annotations.BeforeTest;
 
 public class BaseProgram {
     public static WebDriver driver;
+
     @BeforeTest
     public void setUp(){
+        Logger logger = LogManager.getLogger(BaseProgram.class);
+
         WebDriverManager.chromedriver().setup();
+        //Launch the chrome browser
+        logger.debug("Launch chrome browser");
         driver = new ChromeDriver();
+        //Manage the window
+        logger.debug("Manage Window");
         driver.manage().window().maximize();
+        //Enter the url
+        logger.debug("Opening Website");
         driver.get("https://www.facebook.com/");
 
     }
 
     @AfterTest
     public void tearDown(){
-
+        //close the browser
         driver.close();
+        //quit the browser
         driver.quit();
     }
 }
